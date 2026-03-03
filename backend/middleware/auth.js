7a928e65239d7f1b6e467a6a085e7da7ea4ceb58
Shubh2r <cartoonlover0908@gmail.com>
2026-03-03 23:30:02 +0530

    Implement JWT authentication middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(403).send('Token required');
+  jwt.verify(token, 'secretKey', (err, decoded) => {
+    if (err) return res.status(401).send('Invalid token');
+    req.user = decoded;
+    next();
+  });
+}
+
+function checkRole(roles) {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) return res.status(403).send('Access denied');
+    next();
+  };
+}
+
+module.exports = { verifyToken, checkRole };
 
