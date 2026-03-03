f19bbbd807396a4a1a32d05a8557a280b2d66e04
Shubh2r <cartoonlover0908@gmail.com>
2026-03-04 00:16:01 +0530

    Add links for My Leaves and Manage Leaves

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,9 +8,11 @@ export default function Navbar() {
       <Link to="/profile" style={{ marginRight: '15px' }}>My Profile</Link>
       <Link to="/query" style={{ marginRight: '15px' }}>Raise Query</Link>
       <Link to="/my-queries" style={{ marginRight: '15px' }}>My Queries</Link>
+      <Link to="/leave" style={{ marginRight: '15px' }}>My Leaves</Link>
       <Link to="/admin" style={{ marginRight: '15px' }}>Admin Dashboard</Link>
       <Link to="/profile-approval" style={{ marginRight: '15px' }}>Profile Approvals</Link>
       <Link to="/query-management" style={{ marginRight: '15px' }}>Manage Queries</Link>
+      <Link to="/leave-management" style={{ marginRight: '15px' }}>Manage Leaves</Link>
     </nav>
   );
 }
