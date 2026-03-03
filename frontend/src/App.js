113a7e30c039cc4a28b6e144b354c4e90bc4aa63
Shubh2r <cartoonlover0908@gmail.com>
2026-03-03 23:55:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,19 @@ import EmployeePortal from './pages/EmployeePortal';
 import AdminPortal from './pages/AdminPortal';
 import Profile from './pages/Profile';
 import ProfileApproval from './pages/ProfileApproval';
+import Query from './pages/Query';   // ✅ NEW
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />   {/* ✅ Navigation bar added */}
+      <Navbar />
       <Routes>
         <Route path="/employee" element={<EmployeePortal />} />
         <Route path="/admin" element={<AdminPortal />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/profile-approval" element={<ProfileApproval />} />
+        <Route path="/query" element={<Query />} />   {/* ✅ NEW */}
       </Routes>
     </BrowserRouter>
   );
