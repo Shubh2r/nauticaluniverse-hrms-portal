5a9f1a6afac6dc3c66edc8923584d8ce68a630ec
Shubh2r <cartoonlover0908@gmail.com>
2026-03-03 23:30:34 +0530

    Add routing for Employee and Admin portals

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmployeePortal from './pages/EmployeePortal';
+import AdminPortal from './pages/AdminPortal';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/employee" element={<EmployeePortal />} />
+        <Route path="/admin" element={<AdminPortal />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
