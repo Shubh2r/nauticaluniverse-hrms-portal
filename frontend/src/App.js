b8aa13a0cf6b15c27b4cb01b442d22536a29d081
Shubh2r <cartoonlover0908@gmail.com>
2026-03-03 23:51:20 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EmployeePortal from './pages/EmployeePortal';
 import AdminPortal from './pages/AdminPortal';
+import Profile from './pages/Profile';
+import ProfileApproval from './pages/ProfileApproval';
 
 function App() {
   return (
@@ -9,10 +11,11 @@ function App() {
       <Routes>
         <Route path="/employee" element={<EmployeePortal />} />
         <Route path="/admin" element={<AdminPortal />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile-approval" element={<ProfileApproval />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
