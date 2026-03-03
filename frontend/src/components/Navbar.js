fbddc4fd10543f2761db805761f277c0f7aaa4da
Shubh2r <cartoonlover0908@gmail.com>
2026-03-03 23:52:22 +0530

    Add Navbar component with navigation links

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '10px', background: '#f0f0f0' }}>
+      <Link to="/employee" style={{ marginRight: '15px' }}>Employee Dashboard</Link>
+      <Link to="/profile" style={{ marginRight: '15px' }}>My Profile</Link>
+      <Link to="/query" style={{ marginRight: '15px' }}>Raise Query</Link>
+      <Link to="/admin" style={{ marginRight: '15px' }}>Admin Dashboard</Link>
+      <Link to="/profile-approval" style={{ marginRight: '15px' }}>Profile Approvals</Link>
+    </nav>
+  );
+}
