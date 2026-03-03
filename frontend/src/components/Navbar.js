038f3563ec49c361bd9baf53d36fdbea415805b8
Shubh2r <cartoonlover0908@gmail.com>
2026-03-04 00:01:17 +0530

    Add 'My Queries' link to Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,7 @@ export default function Navbar() {
       <Link to="/employee" style={{ marginRight: '15px' }}>Employee Dashboard</Link>
       <Link to="/profile" style={{ marginRight: '15px' }}>My Profile</Link>
       <Link to="/query" style={{ marginRight: '15px' }}>Raise Query</Link>
+      <Link to="/my-queries" style={{ marginRight: '15px' }}>My Queries</Link>
       <Link to="/admin" style={{ marginRight: '15px' }}>Admin Dashboard</Link>
       <Link to="/profile-approval" style={{ marginRight: '15px' }}>Profile Approvals</Link>
       <Link to="/query-management" style={{ marginRight: '15px' }}>Manage Queries</Link>
