032c77d9d7ca3f004ef43b6dbbf63f395d18f141
Shubh2r <cartoonlover0908@gmail.com>
2026-03-04 00:18:26 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,6 +9,7 @@ export default function Navbar() {
       <Link to="/query" style={{ marginRight: '15px' }}>Raise Query</Link>
       <Link to="/my-queries" style={{ marginRight: '15px' }}>My Queries</Link>
       <Link to="/leave" style={{ marginRight: '15px' }}>My Leaves</Link>
+      <Link to="/calendar" style={{ marginRight: '15px' }}>My Calendar</Link>
       <Link to="/admin" style={{ marginRight: '15px' }}>Admin Dashboard</Link>
       <Link to="/profile-approval" style={{ marginRight: '15px' }}>Profile Approvals</Link>
       <Link to="/query-management" style={{ marginRight: '15px' }}>Manage Queries</Link>
